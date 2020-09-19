16c52696d081dfe57fcc6378c6657bacb0d8c03b
Chi Su <chi.su@uber.com>
2020-09-19 11:03:24 -0700

    format

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 
 // --> 7)  Mount the Logger middleware here
 app.use(function (req, res, next) {
-	console.log(`${req.method} ${req.path} - ${req.ip}`)
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
   next()
 })
 
@@ -17,7 +17,7 @@ console.log("Hello World");
 
 /** 3) Serve an HTML file */
 app.get("/", function (req, res) {
-	res.sendFile(__dirname + "/views/index.html")
+  res.sendFile(__dirname + "/views/index.html")
 });
 
 
@@ -30,9 +30,9 @@ app.use(express.static(__dirname + "/public"));
 /** 6) Use the .env file to configure the app */
 app.get("/json", function (req, res) {
   var message = (process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json");
-	res.json({"message": message});
-}); 
- 
+  res.json({ "message": message });
+});
+
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
@@ -41,20 +41,20 @@ app.get("/now", function (req, res, next) {
   req.time = new Date().toString();
   next();
 }, function (req, res) {
-  res.json({"time": req.time});
+  res.json({ "time": req.time });
 });
 
 /** 9)  Get input from client - Route parameters */
 app.get("/:word/echo", function (req, res) {
-	res.json({"echo": req.params.word});
+  res.json({ "echo": req.params.word });
 });
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 app.get("/name", function (req, res) {
-  res.json({"name": `${req.query.first} ${req.query.last}`});
+  res.json({ "name": `${req.query.first} ${req.query.last}` });
 });
-  
+
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
 
@@ -69,4 +69,4 @@ app.get("/name", function (req, res) {
 
 //---------- DO NOT EDIT BELOW THIS LINE --------------------
 
- module.exports = app;
+module.exports = app;
