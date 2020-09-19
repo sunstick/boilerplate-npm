f68041124c7ced33657ad07522bb4d2203b0dea7
Chi Su <sunstick@gmail.com>
2020-09-19 17:54:01 +0000

    Get input from client - Route parameters

--- myApp.js
+++ myApp.js
@@ -45,7 +45,9 @@ app.get("/now", function (req, res, next) {
 });
 
 /** 9)  Get input from client - Route parameters */
-
+app.get("/:word/echo", function (req, res) {
+	res.json({"echo": req.params.word});
+});
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
