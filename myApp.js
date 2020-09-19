c19ea1df62273a3f05e460c9f76cea60822ae8a4
Chi Su <chi.su@uber.com>
2020-09-19 10:42:49 -0700

    add logging middleware

--- myApp.js
+++ myApp.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
-
+app.use(function (req, res, next) {
+	console.log(`${req.method} ${req.path} - ${req.ip}`)
+})
 
 // --> 11)  Mount the body-parser middleware  here
 
